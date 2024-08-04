154f46f8553a822eb5590a78b92c042cb5d871b5
Ericcost <eric.costerousse@laposte.net>
2024-08-04 14:33:57 +0200

    feature: create a trip model

---
+++ lib/features.trip/data/models/trip_model.dart
@@ -0,0 +1,46 @@
+import 'package:hive/hive.dart';
+
+import '../../domain/entities/trip.dart';
+
+@HiveType(typeId: 0)
+
+class TripModel {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final List<String> pictures;
+  @HiveField(2)
+  final String description;
+  @HiveField(3)
+  final DateTime date;
+  @HiveField(4)
+  final String location; // a enum can be use here if you want to retrieve lattitude and longitude.
+
+  TripModel({
+    required this.title,
+    required this.pictures,
+    required this.description,
+    required this.date,
+    required this.location
+  });
+
+  // Convert from Entity to Model
+  factory TripModel.fromEntity(Trip trip) => TripModel (
+    title: trip.title,
+    pictures: trip.pictures,
+    description: trip.description,
+    date: trip.date,
+    location: trip.location
+  );
+
+  // Convert from Model to Entity
+  Trip toEntity() => Trip(
+    title: title,
+    pictures: pictures,
+    description: description,
+    date: date,
+    location: location
+  );
+
+
+}
